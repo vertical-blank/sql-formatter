d35b59a516d9eb4742c76a3f212cbab745fc1a4d
yohei.yamana <fixeight@gmail.com>
2021-03-01 16:32:42 +0900

    handles floats as single tokens

--- src/main/java/com/github/vertical_blank/sqlformatter/core/Tokenizer.java
+++ src/main/java/com/github/vertical_blank/sqlformatter/core/Tokenizer.java
@@ -49,7 +49,7 @@ public class Tokenizer {
 	 */
 	public Tokenizer(DialectConfig cfg) {
 		this.WHITESPACE_PATTERN = Pattern.compile("^(\\s+)");
-		this.NUMBER_PATTERN = Pattern.compile("^((-\\s*)?[0-9]+(\\.[0-9]+)?|0x[0-9a-fA-F]+|0b[01]+)\\b");
+		this.NUMBER_PATTERN = Pattern.compile("^((-\\s*)?[0-9]+(\\.[0-9]+)?([eE]-?[0-9]+(\\.[0-9]+)?)?|0x[0-9a-fA-F]+|0b[01]+)\\b");
 		this.OPERATOR_PATTERN = Pattern.compile(RegexUtil.createOperatorRegex(
 			new JSLikeList<>(Arrays.asList(
 				"<>",
