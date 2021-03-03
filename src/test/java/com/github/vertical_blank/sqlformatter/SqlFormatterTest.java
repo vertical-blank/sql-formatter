f84b9fd0d123fc4f9f47e2944e00e3b06b01cb23
yohei.yamana <fixeight@gmail.com>
2021-03-03 20:20:10 +0900

    test in Java

---
+++ src/test/java/com/github/vertical_blank/sqlformatter/SqlFormatterTest.java
@@ -0,0 +1,78 @@
+package com.github.vertical_blank.sqlformatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.github.vertical_blank.sqlformatter.languages.Dialect;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class SqlFormatterTest {
+
+  @Test
+  public void simple() {
+    String format =
+        SqlFormatter.format(
+            "SELECT foo, bar, CASE baz WHEN 'one' THEN 1 WHEN 'two' THEN 2 ELSE 3 END FROM table");
+    assertEquals(
+        format,
+        "SELECT\n"
+            + "  foo,\n"
+            + "  bar,\n"
+            + "  CASE\n"
+            + "    baz\n"
+            + "    WHEN 'one' THEN 1\n"
+            + "    WHEN 'two' THEN 2\n"
+            + "    ELSE 3\n"
+            + "  END\n"
+            + "FROM\n"
+            + "  table");
+  }
+
+  @Test
+  public void withIndent() {
+    String format =
+        SqlFormatter.format(
+            "SELECT foo, bar, CASE baz WHEN 'one' THEN 1 WHEN 'two' THEN 2 ELSE 3 END FROM table",
+            "    ");
+    assertEquals(
+        format,
+        "SELECT\n"
+            + "    foo,\n"
+            + "    bar,\n"
+            + "    CASE\n"
+            + "        baz\n"
+            + "        WHEN 'one' THEN 1\n"
+            + "        WHEN 'two' THEN 2\n"
+            + "        ELSE 3\n"
+            + "    END\n"
+            + "FROM\n"
+            + "    table");
+  }
+
+  @Test
+  public void withNamedParams() {
+    Map<String, String> namedParams = new HashMap<>();
+    namedParams.put("foo", "'bar'");
+
+    String format =
+        SqlFormatter.of(Dialect.TSql).format("SELECT * FROM tbl WHERE foo = @foo", namedParams);
+    assertEquals(format, "SELECT\n" + "  *\n" + "FROM\n" + "  tbl\n" + "WHERE\n" + "  foo = 'bar'");
+  }
+
+  @Test
+  public void withFatArrow() {
+    String format =
+        SqlFormatter.modify(config -> config.plusOperators("=>"))
+            .format("SELECT * FROM tbl WHERE foo => '123'");
+    assertEquals(
+        format, "SELECT\n" + "  *\n" + "FROM\n" + "  tbl\n" + "WHERE\n" + "  foo => '123'");
+  }
+
+  @Test
+  public void withIndexedParams() {
+    String format = SqlFormatter.format("SELECT * FROM tbl WHERE foo = ?", Arrays.asList("'bar'"));
+    assertEquals(format, "SELECT\n" + "  *\n" + "FROM\n" + "  tbl\n" + "WHERE\n" + "  foo = 'bar'");
+  }
+}
